5162c996d76c5392088f336fc2828e08597a40f8
JuanmaPadron <juanmarcialpadron12@gmail.com>
2024-10-10 06:36:05 -0100

    Version formulario 2

--- formulario.php
+++ formulario.php
@@ -1,3 +1,13 @@
+<?php
+    
+
+
+?>  
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +16,32 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    echo("Hola mundo");
-    ?>
+    <h2>Formulario</h2>
+   <form action="formulario.php" method="POST">
+        <input type="hidden" name="paso" value="1" />
+        <?php echo $errores; ?>
+
+        <div class="campo">
+            <label class="<?php echo $error_nombre; ?>" for="nombre">Nombre Usuario:</label>
+            <input type="text" id="nombre" name="nombre" value="<?php echo $nombre; ?>" placeholder="Nombre..."> 
+        </div>
+
+        <div class="campo">
+            <label class="<?php echo $error_email; ?>" for="email">E-mail:</label>
+            <input type="email" id="email" name="email" value="<?php echo $email; ?>" placeholder="E-mail..."> 
+        </div>
+
+        <div class="campo">
+            <label class="<?php echo $error_edad; ?>" for="edad">Teléfono:</label>
+            <input type="text" id="edad" name="edad" value="<?php echo $edad; ?>" placeholder="Edad..."> 
+        </div>
+        <div class="campo">
+            <label class="<?php echo $error_pais; ?>" for="pais">Teléfono:</label>
+            <input type="text" id="pais" name="pais" value="<?php echo $pais; ?>" placeholder="País..."> 
+        </div>
+        <div class="campo">
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
 </body>
 </html>
